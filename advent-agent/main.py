152237dbcc0e631fed25cb624f445701c95fa60f
mfkd <mattkfd@gmail.com>
2024-12-30 20:13:25 +0100

    feat: parse day html for content

--- advent-agent/main.py
+++ advent-agent/main.py
@@ -1,8 +1,9 @@
 import requests
 import argparse
+from lxml import html
 
 
-def request_day(day: int, cookie: str) -> str:
+def request_day(day: int, cookie: str) -> bytes:
     url = f"https://adventofcode.com/2024/day/{day}"
 
     # Your session cookie (found after logging into the site via browser)
@@ -16,9 +17,19 @@ def request_day(day: int, cookie: str) -> str:
     # Check the response
     if response.status_code == 200:
         print("Page fetched successfully!")
-        return response.text  # The HTML content of the page
+        return response.content  # The HTML content of the page
     else:
         raise(Exception(f"Failed to fetch page: {response.status_code}"))
+    
+def parse_day(content: bytes) -> str:
+    # Parse the content of the page
+    tree = html.fromstring(content) # type: ignore
+    articles = tree.xpath('//article[@class="day-desc"]')
+
+    if not articles:
+        raise(Exception("No articles found on the page"))
+    
+    return "".join([article.text_content().strip() for article in articles])
 
 def main():
     parser = argparse.ArgumentParser(description="advent-agent")
@@ -26,7 +37,8 @@ def main():
     args = parser.parse_args()
 
     try:
-        print(request_day(1, args.cookie))
+        all_arcticles = parse_day(request_day(1, args.cookie))
+        print(all_arcticles)
     except Exception as e:
         print(e)
 
